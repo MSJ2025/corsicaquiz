34c7d9f45259977eed94ae76a6d1bf63d513b2f4
MSJ2025 <msj2025@icloud.com>
2025-06-28 21:00:00 +0200

    Ajout de tests pour l'authentification Apple

--- test/login_screen_test.dart
+++ test/login_screen_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:corsicaquiz/screens/login_screen.dart';
 import 'package:corsicaquiz/services/auth_service.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:flutter/foundation.dart';
 
 class MockAuthService extends Mock implements AuthService {}
 
@@ -23,6 +24,23 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: LoginScreen(authService: mockAuth)));
 
     await tester.tap(find.text('Se connecter avec Google'));
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
+
+    expect(find.byType(SnackBar), findsOneWidget);
+    expect(find.text('Échec de la connexion'), findsOneWidget);
+  });
+
+  testWidgets('Affiche un SnackBar si la connexion Apple échoue', (tester) async {
+    final mockAuth = MockAuthService();
+    when(() => mockAuth.signInWithApple()).thenAnswer((_) async => null);
+
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+    addTearDown(() => debugDefaultTargetPlatformOverride = null);
+
+    await tester.pumpWidget(MaterialApp(home: LoginScreen(authService: mockAuth)));
+
+    await tester.tap(find.text('Se connecter avec Apple'));
     await tester.pump();
     await tester.pump(const Duration(milliseconds: 100));
 
